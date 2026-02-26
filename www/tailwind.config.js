c98bbde6616729ff08b56999f3607ad638128631
Sergey Mankovsky <sergey@mankovsky.dev>
2026-02-26 20:44:09 +0100

    Remove pages from tailwind config

--- www/tailwind.config.js
+++ www/tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
     preflight: false,
   },
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
